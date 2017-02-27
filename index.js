00aebee80b12c768bdf44c6da988bdb25e0c7687
Ashique <ashique.mohammed@justride.in>
2017-02-28 00:33:15 +0530

    Array length

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ app.post('/ask', urlencodedParser, (req, res) => {
 
             console.log("memebers",members);
 
-            for (var key = 0; key < members.length - 1; key++) {
+            for (var key = 0; key <= members.length - 1; key++) {
                 // Open the channel for each user
                 console.log("Opening im.open for channel" + members[key])
                 request.get('https://slack.com/api/im.open?token=' + process.env.token + '&user=' + members[key] + '&pretty=1', function (error, status, response) {
